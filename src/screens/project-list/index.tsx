6ecffd3f2a67ca05a89e9c0189fc0acbe2515c4f
mengqilu <mqlu@outlook.com>
2022-06-07 22:13:26 -0700

    style: improve project lists page

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import qs from "qs";
 import { cleanObject, useDebounce, useMount } from "../../utils";
 import { useHttp } from "../../utils/http";
+import styled from "@emotion/styled";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 export const ProjectListScreen = () => {
@@ -27,9 +28,14 @@ export const ProjectListScreen = () => {
   });
 
   return (
-    <div>
+    <Container>
+      <h1>项目列表</h1>
       <SearchPanel users={users} param={param} setParam={setParam} />
       <List users={users} list={list} />
-    </div>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  padding: 3.2rem;
+`;
--- src/screens/project-list/list.tsx
+++ src/screens/project-list/list.tsx
@@ -1,6 +1,7 @@
 import { Table } from "antd";
 import React from "react";
 import { User } from "./search-panel";
+import dayjs from "dayjs";
 
 interface Project {
   id: string;
@@ -8,6 +9,7 @@ interface Project {
   personId: string;
   pin: string;
   organization: string;
+  created: number;
 }
 interface ListProps {
   list: Project[];
@@ -24,12 +26,29 @@ export const List = ({ list, users }: ListProps) => {
           sorter: (a, b) => a.name.localeCompare(b.name),
         },
         {
+          title: "部门",
+          dataIndex: "organization",
+          sorter: (a, b) => a.name.localeCompare(b.name),
+        },
+        {
           title: "负责人",
           render(value, project) {
             return (
               <span>
                 {users.find((user) => user.id === project.personId)?.name ||
                   "未知"}
+              </span>
+            );
+          },
+        },
+        {
+          title: "创建时间",
+          render(value, project) {
+            return (
+              <span>
+                {project.created
+                  ? dayjs(project.created).format("YYYY-MM-DD")
+                  : "无"}
               </span>
             );
           },
