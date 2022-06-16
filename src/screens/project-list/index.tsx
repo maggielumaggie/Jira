3e98624c6c298538de8fb9e294ed5346147be798
mengqilu <mqlu@outlook.com>
2022-06-16 15:04:29 -0700

    feat: use user-select.tsx to choose user

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -17,16 +17,15 @@ import { useProjects } from "../../utils/project";
 import { useUsers } from "../../utils/user";
 import { Test } from "../../components/test-closure";
 import { useUrlQueryParam } from "../../utils/url";
+import { useProjectsSearchParams } from "./util";
 // import {Helmet} from 'react-helmet';
 
 const apiUrl = process.env.REACT_APP_API_URL;
 export const ProjectListScreen = () => {
-  const [param, setParam] = useUrlQueryParam(["name", "personId"]);
-  const debouncedParam = useDebounce(param, 200);
-  const { isLoading, error, data: list } = useProjects(debouncedParam);
-  const { data: users } = useUsers();
-
   useDocumentTitle("项目列表", false);
+  const [param, setParam] = useProjectsSearchParams();
+  const { isLoading, error, data: list } = useProjects(useDebounce(param, 200));
+  const { data: users } = useUsers();
 
   return (
     <Container>
--- src/screens/project-list/search-panel.tsx
+++ src/screens/project-list/search-panel.tsx
@@ -1,9 +1,11 @@
 import { jsx } from "@emotion/react";
 import React from "react";
 import { Form, Input, Select } from "antd";
+import { Project } from "./list";
+import { UserSelect } from "../../components/user-select";
 
 export interface User {
-  id: string;
+  id: number;
   name: string;
   email: string;
   title: string;
@@ -12,10 +14,7 @@ export interface User {
 }
 interface SearchPanelProps {
   users: User[];
-  param: {
-    name: string;
-    personId: string;
-  };
+  param: Partial<Pick<Project, "name" | "personId">>;
   setParam: (param: SearchPanelProps["param"]) => void;
 }
 export const SearchPanel = ({ users, param, setParam }: SearchPanelProps) => {
@@ -35,7 +34,8 @@ export const SearchPanel = ({ users, param, setParam }: SearchPanelProps) => {
         />
       </Form.Item>
       <Form.Item>
-        <Select
+        <UserSelect
+          defaultOptionName={"负责人"}
           value={param.personId}
           onChange={(value) =>
             setParam({
@@ -43,14 +43,7 @@ export const SearchPanel = ({ users, param, setParam }: SearchPanelProps) => {
               personId: value,
             })
           }
-        >
-          <Select.Option value={""}>负责人</Select.Option>
-          {users.map((user) => (
-            <Select.Option key={user.name} value={String(user.id)}>
-              {user.name}
-            </Select.Option>
-          ))}
-        </Select>
+        />
       </Form.Item>
     </Form>
   );
